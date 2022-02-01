d44e1c227c1a25999be4d79ee05cd0dbe8eae79f
James Gilmore <gilmorej@users.noreply.github.com>
2022-02-01 09:51:55 -0700

    Update Types.h

--- Types.h
+++ Types.h
@@ -1,8 +1,10 @@
 #pragma once
 
 struct particleSensorState_t {
-    uint16_t avgPM25 = 0;
-    uint16_t measurements[5] = {0, 0, 0, 0, 0};
-    uint8_t measurementIdx = 0;
+    float pm25 = 0;
+    float pm10 = 0;
+    float temp = 0;
+    float humidity = 0;
+    float heatIndex = 0;
     boolean valid = false;
 };
