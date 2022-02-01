6fcba0446f4227141534604b09e96d108c2c27d2
James Gilmore <gilmorej@users.noreply.github.com>
2022-02-01 09:51:41 -0700

    Update SerialCom.h

--- SerialCom.h
+++ SerialCom.h
@@ -1,114 +1,74 @@
 #pragma once
 
 #include <SoftwareSerial.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
 #include "Types.h"
 
+#define DHT_TYPE DHT22
+
 namespace SerialCom {
-    constexpr static const uint8_t PIN_UART_RX = 4; // D2 on Wemos D1 Mini
-    constexpr static const uint8_t PIN_UART_TX = 13; // UNUSED
+    constexpr static const uint8_t SDS_TX_PIN = 0; // D3 on Wemos D1 Mini -> SDS011 TX pin
+    constexpr static const uint8_t SDS_RX_PIN = 2; // D4 on Wemos D1 Mini -> SDS011 RX pin
+    constexpr static const uint8_t DHT_PIN = 5;    // D1 on Wemos D1 Mini -> DHT22 Data Pin
 
-    SoftwareSerial sensorSerial(PIN_UART_RX, PIN_UART_TX);
+    SoftwareSerial sensorSerial(SDS_RX_PIN, SDS_TX_PIN);
+    SdsDustSensor sds(sensorSerial); //  additional parameters: retryDelayMs and maxRetriesNotAvailable
+    const int MINUTE = 60000;
+    const int WAKEUP_WORKING_TIME = 30000; // 30 seconds.
+    const int MEASUREMENT_INTERVAL = MINUTE;
+    const int CYCLE_MINUTES = 10;
 
-    uint8_t serialRxBuf[255];
-    uint8_t rxBufIdx = 0;
-
+    DHT dht(DHT_PIN, DHT_TYPE);
+        
     void setup() {
-        sensorSerial.begin(9600);
+      Serial.begin(115200);
+      delay(5000);
+      /* SDS011 Dust Sensor */
+      sds.begin();
+      dht.begin();
+      delay(5000);
+      // Prints SDS011 firmware version:
+      Serial.println("SDS011 Initialize");
+      Serial.println(sds.queryFirmwareVersion().toString());
+      // Ensures SDS011 is in 'query' reporting mode:
+      Serial.println(sds.setQueryReportingMode().toString());
+      WorkingPeriodResult workingPeriodResult = sds.setCustomWorkingPeriod(CYCLE_MINUTES);
+      Serial.print("SDS011 Working Period Configuration: ");
+      Serial.println(workingPeriodResult.toString()); 
     }
 
-    void clearRxBuf() {
-        // Clear everything for the next message
-        memset(serialRxBuf, 0, sizeof(serialRxBuf));
-        rxBufIdx = 0;
-    }
-
-    void parseState(particleSensorState_t& state) {
-        /**
-         *         MSB  DF 3     DF 4  LSB
-         * uint16_t = xxxxxxxx xxxxxxxx
-         */
-        const uint16_t pm25 = (serialRxBuf[5] << 8) | serialRxBuf[6];
-
-        Serial.printf("Received PM 2.5 reading: %d\n", pm25);
-
-        state.measurements[state.measurementIdx] = pm25;
-
-        state.measurementIdx = (state.measurementIdx + 1) % 5;
-
-        if (state.measurementIdx == 0) {
-            float avgPM25 = 0.0f;
-
-            for (uint8_t i = 0; i < 5; ++i) {
-                avgPM25 += state.measurements[i] / 5.0f;
-            }
-
-            state.avgPM25 = avgPM25;
-            state.valid = true;
-
-            Serial.printf("New Avg PM25: %d\n", state.avgPM25);
-        }
-
-        clearRxBuf();
-    }
-
-    bool isValidHeader() {
-        bool headerValid = serialRxBuf[0] == 0x16 && serialRxBuf[1] == 0x11 && serialRxBuf[2] == 0x0B;
-
-        if (!headerValid) {
-            Serial.println("Received message with invalid header.");
-        }
-
-        return headerValid;
-    }
-
-    bool isValidChecksum() {
-        uint8_t checksum = 0;
-
-        for (uint8_t i = 0; i < 20; i++) {
-            checksum += serialRxBuf[i];
-        }
-
-        if (checksum != 0) {
-            Serial.printf("Received message with invalid checksum. Expected: 0. Actual: %d\n", checksum);
-        }
-
-        return checksum == 0;
-    }
-
-    void handleUart(particleSensorState_t& state) {
-        if (!sensorSerial.available()) {
-            return;
-        }
-
-        Serial.print("Receiving:");
-        while (sensorSerial.available()) {
-            serialRxBuf[rxBufIdx++] = sensorSerial.read();
-            Serial.print(".");
-
-            // Without this delay, receiving data breaks for reasons that are beyond me
-            delay(15);
-
-            if (rxBufIdx >= 64) {
-                clearRxBuf();
-            }
-        }
-        Serial.println("Done.");
-
-        if (isValidHeader() && isValidChecksum()) {
-            parseState(state);
-
-            Serial.printf(
-                "Current measurements: %d, %d, %d, %d, %d\n",
-
-                state.measurements[0],
-                state.measurements[1],
-                state.measurements[2],
-                state.measurements[3],
-                state.measurements[4]
-            );
+    void collectParticles(particleSensorState_t& state) {
+        // Wake up SDS011
+        sds.wakeup();
+        delay(WAKEUP_WORKING_TIME);
+        // Get data from SDS011
+        PmResult pm = sds.queryPm();
+        if (pm.isOk()) {
+          state.pm25 = pm.pm25;
+          state.pm10 = pm.pm10;
+          state.valid = true;
         } else {
-            clearRxBuf();
+          state.valid = false;
+          Serial.print("Could not read values from sensor, reason: ");
+          Serial.println(pm.statusToString());
         }
+        // Put SDS011 back to sleep
+        WorkingStateResult workingStateResult = sds.sleep();
+        if (workingStateResult.isWorking()) {
+          Serial.println("Problem with sleeping the SDS011 sensor.");
+        } else {
+          Serial.println("SDS011 sensor is now sleeping");
+        }
+    }
+
+    void collectTemp(particleSensorState_t& state) { 
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      float hif = dht.computeHeatIndex(t, h);
+      state.temp = t;
+      state.humidity = h;
+      state.heatIndex = hif;
     }
 } // namespace SerialCom
